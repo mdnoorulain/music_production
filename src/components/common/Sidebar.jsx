9e117f02ae77e891a80d7e3355b8a1a20701b55c
mdnoorulain <mdnoorulain786.mna@gmail.com>
2024-03-15 15:11:14 +0530

    first push

---
+++ src/components/common/Sidebar.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import { recommand } from "../assets/data/data"
+import { Card_sm } from "./Card_sm"
+
+export const Sidebar = () => {
+  return (
+    <>
+      <section className='sidebar hero'>
+        <h1 className='mb-5 text-lg font-semibold text-gray-600'>5 Likes</h1>
+        {recommand.slice(0, 5).map((item, i) => (
+          <div className='mb-3'>
+            <Card_sm cover={item.cover} name={item.name} tag={item.tag} i={i} />
+          </div>
+        ))}
+        <p className='text-sm mt-3'>About Contact Legal Policy</p>
+        <span className='text-gray-500 text-[12px]'>© Copyright 2024</span>
+        <a href="https://github.com/mdnoorulain" 
+        className=' pl-1 cursor-pointer text-[12px] text-gray-500 hover:text-gray-900'
+        target='_blank' rel='noopener noreferrer'>
+              Noorul Ain</a>
+      </section>
+    </>
+  )
+}
